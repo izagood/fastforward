00a6b3f1e1032182214e9f46e17834f8f34968a2
izagood <ljbfif50@gmail.com>
2022-12-02 02:59:21 +0900

    controller openapi 설명 추가

--- src/main/kotlin/com/fastforward/stock/controller/StockController.kt
+++ src/main/kotlin/com/fastforward/stock/controller/StockController.kt
@@ -1,6 +1,11 @@
 package com.fastforward.stock.controller
 
 import com.fastforward.stock.service.StockService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -9,7 +14,22 @@ import org.springframework.web.bind.annotation.*
 class StockController(
     private val stockService: StockService,
 ) {
-
+    @Operation(
+        description = "주식 조회",
+        responses = [
+            ApiResponse(responseCode = "200", description = "성공"),
+            ApiResponse(
+                responseCode = "400",
+                description = "실패",
+                content = [Content(examples = [ExampleObject("stock 정보가 없습니다.")])]
+            )
+        ],
+        parameters = [
+            Parameter(name = "symbol", description = "상장코드", example = "005930.KS"),
+            Parameter(name = "interval", description = "조회 간격", example = "1d"),
+            Parameter(name = "range", description = "조회 범위", example = "5d"),
+        ]
+    )
     @GetMapping("/{symbol}/{interval}/{range}")
     fun getStock(
         @PathVariable("symbol") symbol: String,
